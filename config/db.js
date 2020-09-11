21193276fd7117dc7b14e0fd657fabef6ba37d81
Steven Polido <steven.polido@vistage.com>
2020-09-11 11:23:37 -0700

    Modified

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require("sequelize");
 const env = require("dotenv").config();
-
     
     const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
         host: 'ec2-34-195-115-225.compute-1.amazonaws.com',
