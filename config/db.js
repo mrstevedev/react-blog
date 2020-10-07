ae4bcea13cdb0e45f01610b741286742c281e5d3
Steven Polido <steven.polido@vistage.com>
2020-10-07 15:52:32 -0700

    Updated host value

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require("sequelize");
 const env = require("dotenv").config();
+
     
-    const db = new Sequelize(process.env.DATABASE_URL, {
-        host: 'ec2-34-195-115-225.compute-1.amazonaws.com',
+    const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: process.env.DB_HOST,
         dialect: 'postgres',
-        port: '53659',
-        omitNull: true
+        port: '53659'
     });
 
     db.authenticate()
-        .then(() => console.log('Database connected successfully.'))
+        .then(() => console.log(`Database connected to ${process.env.DB_NAME} successfully.`))
         .catch(err => console.log(err));
 
 
