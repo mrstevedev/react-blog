6402941a5c405ddfa6e3e1e62d12cdc1eeff9f01
Steven Polido <steven.polido@vistage.com>
2020-11-23 15:16:25 -0800

    Added LatestPost.js component

---
+++ client/src/components/LatestPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LatestPost extends Component {
+    render() {
+        return (
+            <div>
+                latest post
+            </div>
+        )
+    }
+}
+
+export default LatestPost
