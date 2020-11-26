fb0c023860e76b52242c3d65b7486675035d9de5
Steven Polido <steven.polido@vistage.com>
2020-11-25 22:57:22 -0800

    Added Footer.js component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div>
+            <p>
+                Blog developed by sp. Developed with React.js
+            </p>
+        </div>
+    )
+}
