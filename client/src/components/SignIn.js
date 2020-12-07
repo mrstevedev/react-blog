7ed44e5ade96319a2b42c5e67f2769ecb54a2dcb
Steven Polido <steven.polido@vistage.com>
2020-12-07 15:55:56 -0800

    Added SignIn.js component

---
+++ client/src/components/SignIn.js
@@ -0,0 +1,148 @@
+import React, { useState, Fragment } from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import {Link} from 'react-router-dom';
+import Snackbar from '@material-ui/core/Snackbar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MuiAlert from "@material-ui/lab/Alert";
+
+import { mutate } from "swr";
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(3),
+      marginTop: '0.4rem',
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      whiteSpace: 'nowrap',
+    },
+    paperContent: {
+      padding: '3rem 8rem',
+      margin: '1rem 1rem 0 1rem',
+      textAlign: 'left',
+      color: theme.palette.text.secondary,
+      whiteSpace: 'nowrap',
+      height: '96%',
+      '@media(max-width: 768px)': {
+        padding: '3rem 2rem !important',      
+        whiteSpace: 'normal !important',
+      }
+    },
+    title: {
+      flexGrow: 1,
+    },
+    postText: {
+      padding: '0.4rem 0 1rem'
+    },
+    postDate: {
+        fontWeight: '500',
+        fontSize: '12px'
+    },
+    showCount: {
+        fontSize: '14px'
+    },
+    successMsg: {
+        fontWeight: 'bold',
+        color: 'cadetblue',
+        padding: '2rem 0'
+    },
+    postContent: {
+        padding:  '2rem 2rem',
+        '@media(min-width: 768px)': {
+            padding:  '4rem 8rem',
+            width: '1200px'
+          }
+    },
+    relatedPosts: {
+        display: 'flex',
+        overflow: 'scroll'
+    },
+    chip: {
+        margin: '0 0.5rem 0 0',
+        cursor: 'pointer',
+        fontWeight: 'bold',
+        color: '#797979',
+        '&:hover': {
+            background: '#3f51b5',
+            color: '#fff'
+        }
+    },
+    adminText: {
+        color: '#ccc'
+    },
+    dialogPaper: {
+        padding: '3rem 0 0 0',
+        width: '600px',
+        height : '500px'
+    },
+    textField: {
+        display: 'block',
+        margin: '1.4rem'
+    },
+    profileLabel: {
+        color: '#a5a5a5',
+        margin: '1rem 0',
+        fontSize: '0.76rem',
+        textTransform: 'uppercase'
+    },
+    profileRow: {
+        margin: '2rem 0'
+    },
+    inputStyle: {
+        background: 'transparent',
+        border: 'none',
+        outline: 'none'
+    },
+    linkText: {
+        color: "#3f51b5",
+        margin: '0 2rem 0 0'
+      },
+  }));
+
+function Profile(props) {
+    const classes = useStyles();
+
+    const [toast, setToastState] = useState({
+        open: false,
+        vertical: 'top',
+        horizontal: 'center',
+    });
+
+    return(
+        <Fragment>
+          <div className="container">
+                <h3 style={{ fontSize: '2.4rem', fontWeight: 'bold', color: '#000'}}>Profile<span className={classes.adminText}> | SignIn</span></h3>                        
+                    <div>
+                        <p style={{
+                            width: '100%',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis', fontSize: '1.3rem', fontWeight: '500', color: '#888', lineHeight: '43px' }}
+                            ></p>
+                        <div style={{ padding: '2rem 0', borderRadius: '4px', margin: '0rem 0' }}>
+                            <div className={ classes.profileRow }>
+                            <h2 className={ classes.profileLabel }>Username:</h2>
+                            <input type="text" placeholder="Jay Gordon" className={classes.inputStyle} />
+                            </div>
+                           <div className={ classes.profileRow }>
+                           <h2 className={ classes.profileLabel }>Password:</h2>
+                            <input type="password" placeholder="******" className={classes.inputStyle} />
+                           </div>
+                           
+                           <Link className={`link ${ classes.linkText }`} to="#">Forgot password?</Link> 
+                            <Button variant="contained" color="primary">
+                                Sign-In
+                            </Button>                            
+                        </div>
+                </div>                        
+            </div>    
+        </Fragment>
+    );
+}
+export default Profile;
